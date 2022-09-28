3112cf4099b4c3729f0990c75df36a99187e19c6
Luka D <lukadimopoulos@gmail.com>
2022-09-28 11:59:12 +1000

    Start of Week 2 Activities

---
+++ Assets/Scripts/Week2Activities.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace LukaDimopoulos
+{
+    public class Week2Activities : MonoBehaviour
+    {
+        private string favouriteGame = "Borderlands 2";
+        private int hoursPlayed = 1672;
+        private float costOfGame = 25f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("My favourite game is " + favouriteGame + "!");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
